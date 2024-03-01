b8f0b346becb46be0421faf7938b952c65a14d90
Lenovo <Lenovo@DESKTOP-KFG6V88>
2024-02-29 20:03:43 -0600

    Agregar archivos de proyecto.

---
+++ Controllers/BuscadorController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Searchamb.Controllers
+{
+    public class BuscadorController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Searchamb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
